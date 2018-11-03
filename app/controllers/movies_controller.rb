b2d8252958b7a5f02ace910a0dc363c314e183cf
EC2 Default User <ec2-user@ip-172-31-33-35.us-east-2.compute.internal>
2018-11-03 03:56:41 +0000

    add rescue for invalid movie id in show method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,14 @@ class MoviesController < ApplicationController
   
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    # if id > Movie.all.count
+    #   redirect('/movie')
+    # end
+    begin
+      @movie = Movie.find(id) # look up movie by unique ID
+    rescue ActiveRecord::RecordNotFound
+      redirect_to :action => 'index'
+    end
     # will render app/views/movies/show.html.haml by default
   end
 end
