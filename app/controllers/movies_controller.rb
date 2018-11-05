60e3c9cccd42d5a8e087bec538ae4e5b3f997104
EC2 Default User <ec2-user@ip-172-31-33-35.us-east-2.compute.internal>
2018-11-05 04:40:42 +0000

    edit and delete movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,10 +18,29 @@ class MoviesController < ApplicationController
     # default: render 'new' template
   end
   
-  # in movies_controller.rb
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+ 
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(movie_params)
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  } # as before
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
   
