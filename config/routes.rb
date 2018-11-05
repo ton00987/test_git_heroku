f27e4c11844b478eea4958339cd7b724353b89ef
EC2 Default User <ec2-user@ip-172-31-33-35.us-east-2.compute.internal>
2018-11-05 09:46:51 +0000

    can login but not proper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   root :to => redirect('/movies')
-  get 'login_github', to: redirect('/auth/github')
+  get 'login', to: redirect('/auth/github')
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout' => 'sessions#destroy'
   get 'auth/failure' => 'sessions#failure'
